d846f90f4e581ab7b92c84a2d27dd82feaba33a1
gabrielbarth <gabriel.silverio@stone.com.br>
2024-07-21 16:50:49 -0400

    chore: create src/App and set prettier bracketSpacing

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { ReactElement } from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+const App = (): ReactElement => {
+  return (
+    <SafeAreaView>
+      <Text>oi</Text>
+    </SafeAreaView>
+  );
+};
+
+export { App };
